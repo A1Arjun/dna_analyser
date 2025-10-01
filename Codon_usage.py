c6ca6096e1f7904f6570d9cc332bfba9a855ec5d
Arjun Kumar Kushwaha <arjunkumarkushwaha001@gmail.com>
2025-10-01 17:11:58 +0530

    Create Codon_usage.py

---
+++ Codon_usage.py
@@ -0,0 +1,26 @@
+from Bio import SeqIO
+
+# Function to split sequence into codons
+def get_codons(seq):
+    codons = [str(seq[i:i+3]) for i in range(0, len(seq)-2, 3)]
+    return codons
+
+# Dictionary to store codon counts
+codon_counts = {}
+
+# Read sequences from FASTA
+for record in SeqIO.parse("multiseq.fasta", "fasta"):
+    seq = record.seq.upper()
+    codons = get_codons(seq)
+    for codon in codons:
+        if codon in codon_counts:
+            codon_counts[codon] += 1
+        else:
+            codon_counts[codon] = 1
+
+# Print codon usage
+total_codons = sum(codon_counts.values())
+print("Codon\tCount\tFrequency")
+for codon in codon_counts:
+    frequency = codon_counts[codon] / total_codons
+    print(f"{codon}\t{codon_counts[codon]}\t{frequency:.4f}")
